9f9f3c00f6d47a094cfc72b8d9be28fa9fd7f7e1
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2020-02-26 07:45:34 -0800

    scrolling working

---
+++ Conor of War/Assets/Scripts/Scrolling.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrolling : MonoBehaviour
+{
+    public float speed = 3f;
+    public float cameraRightClamp = 16f;
+    public float cameraLeftClamp = -16f;
+
+    private bool canMoveRight = true;
+    private bool canMoveLeft = true;
+    
+    void Start()
+    {
+        /*
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.None;
+        */
+    }
+
+    void Update()
+    {
+        if(transform.position.x < cameraLeftClamp)
+        {
+            canMoveRight = true;
+            canMoveLeft = false;
+        }
+        else
+            canMoveLeft = true;
+
+        
+        if (transform.position.x > cameraRightClamp)
+        {
+            canMoveRight = false;
+            canMoveLeft = true;
+        }
+        else
+            canMoveRight = true;
+
+
+        //transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed, 0f, 0f);
+
+        if(canMoveRight && Input.GetAxisRaw("Mouse X") > -1)
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0f, 0f);
+        }
+        if (canMoveLeft && Input.GetAxisRaw("Mouse X") < 1)
+        {
+            transform.position -= new Vector3(speed * Time.deltaTime, 0f, 0f);
+        }
+    }
+    /*
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("CameraRigthWall"))
+        {
+            Debug.Log("Right Wall");
+            canMoveRight = false;
+            canMoveLeft = true;
+        }
+        if(other.CompareTag("CameraLeftWall"))
+        {
+            Debug.Log("Left Wall");
+            canMoveRight = true;
+            canMoveLeft = false;
+        }
+    }
+    */
+}
