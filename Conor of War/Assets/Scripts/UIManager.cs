474c508370d5a4e98019e28942f6e38f76d757bf
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2020-03-03 06:24:01 -0800

    Added cooldown to spawn

--- Conor of War/Assets/Scripts/UIManager.cs
+++ Conor of War/Assets/Scripts/UIManager.cs
@@ -8,41 +8,77 @@ public class UIManager : MonoBehaviour
     public GameObject soldier, knight, archer, rogue, wizard;
     public GameObject monsterSpawn, humanSpawn;
     private Vector2 monsterSpawnPos;
-    [SerializeField]private Vector2 humanSpawnPos;
+    [SerializeField] private Vector2 humanSpawnPos;
+
+    public float zombieCooldownTime, werewolfCooldownTime, vampireCooldownTime, skeletonCooldownTime, demonCooldownTime;
 
     void Start()
     {
         monsterSpawnPos = new Vector2(monsterSpawn.transform.position.x, monsterSpawn.transform.position.y);
         humanSpawnPos = new Vector2(humanSpawn.transform.position.x, humanSpawn.transform.position.y);
     }
-    
+
     void Update()
     {
-        
+
     }
 
     public void Zombie()
     {
-        Instantiate(zombie, monsterSpawnPos, zombie.transform.rotation);
+        StartCoroutine(ZombieCoroutine(zombieCooldownTime));
     }
 
     public void Werewolf()
     {
-        Instantiate(werewolf, monsterSpawnPos, zombie.transform.rotation);
+        StartCoroutine(WerewolfCoroutine(werewolfCooldownTime));
     }
 
     public void Vampire()
     {
-        Instantiate(vampire, monsterSpawnPos, zombie.transform.rotation);
+        StartCoroutine(VampireCoroutine(vampireCooldownTime));
     }
 
     public void Skeleton()
     {
-        Instantiate(skeleton, monsterSpawnPos, zombie.transform.rotation);
+        StartCoroutine(SkeletonCoroutine(skeletonCooldownTime));
     }
 
     public void Demon()
     {
+        StartCoroutine(DemonCoroutine(demonCooldownTime));
+    }
+
+
+
+
+
+    IEnumerator ZombieCoroutine(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Instantiate(zombie, monsterSpawnPos, zombie.transform.rotation);
+    }
+
+    IEnumerator WerewolfCoroutine(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Instantiate(werewolf, monsterSpawnPos, zombie.transform.rotation);
+    }
+
+    IEnumerator VampireCoroutine(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Instantiate(vampire, monsterSpawnPos, zombie.transform.rotation);
+    }
+
+    IEnumerator SkeletonCoroutine(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Instantiate(skeleton, monsterSpawnPos, zombie.transform.rotation);
+    }
+
+    IEnumerator DemonCoroutine(float time)
+    {
+        yield return new WaitForSeconds(time);
         Instantiate(demon, monsterSpawnPos, zombie.transform.rotation);
     }
 }
