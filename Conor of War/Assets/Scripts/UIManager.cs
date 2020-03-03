befe135905bbbfbb462fba9057591c96ea291127
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2020-03-03 08:10:08 -0800

    currency reduces when you spawn monsters

--- Conor of War/Assets/Scripts/UIManager.cs
+++ Conor of War/Assets/Scripts/UIManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
@@ -8,44 +9,56 @@ public class UIManager : MonoBehaviour
     public GameObject soldier, knight, archer, rogue, wizard;
     public GameObject monsterSpawn, humanSpawn;
     private Vector2 monsterSpawnPos;
-    [SerializeField] private Vector2 humanSpawnPos;
+    private Vector2 humanSpawnPos;
 
     public float zombieCooldownTime, werewolfCooldownTime, vampireCooldownTime, skeletonCooldownTime, demonCooldownTime;
+    [SerializeField] private float zombieCost = 10f, werewolfCost = 30f, vampireCost = 20f, skeletonCost = 20f, demonCost = 80f;
+
+    public float currency = 100f;
+    public Text currencyText;
+    public string currencyName;
 
     void Start()
     {
         monsterSpawnPos = new Vector2(monsterSpawn.transform.position.x, monsterSpawn.transform.position.y);
         humanSpawnPos = new Vector2(humanSpawn.transform.position.x, humanSpawn.transform.position.y);
+        
+        currencyText.text = currencyName + ": " + currency.ToString(); ///display currency at the top of the screen
     }
 
     void Update()
     {
-
+        currencyText.text = currencyName + ": " + currency.ToString(); ///display currency at the top of the screen
     }
 
     public void Zombie()
     {
         StartCoroutine(ZombieCoroutine(zombieCooldownTime));
+        currency -= zombieCost;
     }
 
     public void Werewolf()
     {
         StartCoroutine(WerewolfCoroutine(werewolfCooldownTime));
+        currency -= werewolfCost;
     }
 
     public void Vampire()
     {
         StartCoroutine(VampireCoroutine(vampireCooldownTime));
+        currency -= vampireCost;
     }
 
     public void Skeleton()
     {
         StartCoroutine(SkeletonCoroutine(skeletonCooldownTime));
+        currency -= skeletonCost;
     }
 
     public void Demon()
     {
         StartCoroutine(DemonCoroutine(demonCooldownTime));
+        currency -= demonCost;
     }
 
 
