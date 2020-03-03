24031c10e44fcb3a3f3f57cf6bfa58b75a9f368f
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2020-03-03 03:51:29 -0800

    new scripts

---
+++ Conor of War/Assets/Scripts/Monsters/Monster.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    private Rigidbody2D myRb;
+
+    public float health;
+    public float cost;
+    public float cooldown;
+    public string speedRate;
+    public float speed;
+    public string characterType;
+    public float damagePerSecond;
+    public float pointPerKill;
+
+    void Start()
+    {
+        myRb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        myRb.velocity = new Vector2(speed, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Demon"))
+        {
+            Debug.Log("Demon");
+        }
+
+        if (collision.CompareTag("Skeleton"))
+        {
+            Debug.Log("Skeleton");
+        }
+
+        if (collision.CompareTag("Vampire"))
+        {
+            Debug.Log("Vampire");
+        }
+
+        if (collision.CompareTag("Werewolf"))
+        {
+            Debug.Log("Werewolf");
+        }
+
+        if (collision.CompareTag("Zombie"))
+        {
+            Debug.Log("Zombie   ");
+        }
+    }
+}
---
+++ Conor of War/Assets/Scripts/Monsters/Skeleton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skeleton : MonoBehaviour
+{
+    public float health = 80f;
+    public float cost = 20f;
+    public float cooldown = 2f;
+    public string speedRate = "Fast";
+    public float speed = 3f;
+    public string characterType = "Melee";
+    public float damagePerSecond = 60f;
+    public float pointPerKill = 30f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
