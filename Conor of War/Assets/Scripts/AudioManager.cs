636dce9ff9188a99b5a4c50905ade1a22bd59138
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2020-03-10 12:43:38 -0800

    Audio Manager

---
+++ Conor of War/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private AudioSource audioS;
+    [SerializeField] private AudioClip button, zombie, vampire, skeleton, demon, werewolf;
+
+    void Start()
+    {
+        audioS = GetComponent<AudioSource>();   
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ButtonClick()
+    {
+        audioS.PlayOneShot(button);
+    }
+    public void Zombie()
+    {
+        audioS.PlayOneShot(zombie);
+    }
+    public void Vampire()
+    {
+        audioS.PlayOneShot(vampire);
+    }
+    public void Skeleton()
+    {
+        audioS.PlayOneShot(skeleton);
+    }
+    public void Demon()
+    {
+        audioS.PlayOneShot(demon);
+    }
+    public void Werewolf()
+    {
+        audioS.PlayOneShot(werewolf);
+    }
+}
